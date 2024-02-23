5021942115fd205da10974b426f8f85dd5ad3c8a
Jacob-Bordelon <jacob.bordelon@iinfosec.com>
2024-02-23 10:34:08 -0600

    use cloud

--- provider.tf
+++ provider.tf
@@ -14,4 +14,13 @@ resource "aws_s3_bucket" "temp3" {
 
 terraform {
   required_version = ">= 0.12"
+  cloud {
+    organization = "aws"
+    hostname = "127.0.0.1"
+
+    workspaces {
+      project = "personal-terraform"
+      tags = ["networking", "source:cli"]
+    }
+  }
 }
