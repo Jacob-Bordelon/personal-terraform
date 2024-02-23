2b4d5e3496e75351a66306920eb612553913c819
Jacob-Bordelon <jacob.bordelon@iinfosec.com>
2024-02-23 08:57:21 -0600

    added cloud block

--- provider.tf
+++ provider.tf
@@ -3,3 +3,16 @@ provider aws {}
 resource "aws_s3_bucket" "temp" {
     bucket = "test"
 }
+
+terraform {
+  required_version = ">= 0.12"
+  cloud {
+    organization = "aws"
+    hostname = "terrakube-ui"
+
+    workspaces {
+        project = "personal-terraform"
+        tags = ["testing", "source:cli"]
+    }
+  }
+}
